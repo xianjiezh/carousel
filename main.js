bf030cf5b93631c125e2907fbc752d7a4f371c0a
xianjiezh <904292766@qq.com>
2017-12-22 23:40:40 +0800

    调整点击事件

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 $('li').on('click',function(event){
     let target = event.target || window.target
     let n = $(target).index()
-    let s = n * (-180) + 'px'
+    let s = n * (-192) + 'px'
     console.log('s:'+ s)
-    $('.pics').css("margin-left",s)
+    $('.pics').css({"margin-left":s,"transition":"all .4s"})
 })
