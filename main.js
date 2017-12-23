a908a79df0ab674ed77ea8c00c9714dab54645f2
xianjiezh <904292766@qq.com>
2017-12-23 22:25:17 +0800

    添加定时器

--- main.js
+++ main.js
@@ -4,6 +4,15 @@ $('li').on('click',function(event){
     let target = event.target || window.target
     let n = $(target).index()
     let s = n * (-192) + 'px'
-    console.log('s:'+ s)
-    $('.pics').css({"margin-left":s,"transition":"all .4s"})
+    $('.pics').css({"margin-left":s,"transition":"all .7s"})
 })
+
+let n = 0
+let timer = setInterval(function(){
+    let len = $('li').length
+    console.log('len:'+len)
+    let num = n % 4
+    console.log('num:'+num)
+    $('li').eq(num).trigger('click')
+    n += 1
+},2000)
