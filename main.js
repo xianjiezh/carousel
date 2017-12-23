8347731d4abbb9414e482f8e3a7e93407c0f7021
xianjiezh <904292766@qq.com>
2017-12-23 22:52:54 +0800

    添加鼠标进入和离开时间

--- main.js
+++ main.js
@@ -1,18 +1,32 @@
 
 
-$('li').on('click',function(event){
+$('li').on('click', function (event) {
     let target = event.target || window.target
     let n = $(target).index()
     let s = n * (-192) + 'px'
-    $('.pics').css({"margin-left":s,"transition":"all .7s"})
+    $('.pics').css({ "margin-left": s, "transition": "all .8s" })
+    $('li').removeClass('active')
+    $('li').eq(n).addClass('active')
 })
 
-let n = 0
-let timer = setInterval(function(){
+
+let n = 1
+function setTimer() {
     let len = $('li').length
-    console.log('len:'+len)
+    console.log('len:' + len)
     let num = n % 4
-    console.log('num:'+num)
+    console.log('num:' + num)
     $('li').eq(num).trigger('click')
     n += 1
-},2000)
+}
+let timer = setInterval(setTimer, 3000)
+
+let picWrapper = document.getElementsByClassName('picsWrapper')[0]
+picWrapper.onmouseenter = function () {
+    console.log('鼠标进入')
+    clearInterval(timer)
+}
+picWrapper.onmouseleave = function () {
+    console.log('鼠标离开')
+    timer = setInterval(setTimer, 3000)
+}
